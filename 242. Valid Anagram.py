5dc8bad764a8cc626f6e8bff26d207758f5c9c4e
alber.aleksandrov <alber.aleksandrov@x5.ru>
2023-01-10 22:00:39 +0500

    Добавлены дополнительные решения к задаче 242

--- 242. Valid Anagram.py
+++ 242. Valid Anagram.py
@@ -18,25 +18,80 @@ Example 2:
 from collections import Counter
 
 
-class Solution1(object):
-    def isAnagram(self, s, t):
+class Solution1:
+    """Решение 1.
+
+    Time: O(n)
+    Space: O(n)
+
+    Предпочтительное решение из-за простоты кода, его минималистичности и читаемости, в нем сложно ошибиться
+
+    Временная сложность обусловлена преобразованием строк в Counter (2n принимаем за n)
+    То же самое и с пространственной сложностью (в худшем случае будет Counter, в котором n элементов)
+
+    """
+
+    def is_anagram(self, s: str, t: str):
         if len(s) != len(t):
             return False
-        s = list(s)
-        s.sort()
-        t = list(t)
-        t.sort()
-        return s == t
+
+        return Counter(s) == Counter(t)
+
+
+solution1 = Solution1()
+
+assert solution1.is_anagram('anagram', 'nagaram') is True
+assert solution1.is_anagram('rat', 'car') is False
 
 
 class Solution2(object):
-    def isAnagram(self, s, t):
+    """Решение 2.
+
+    Time: O(nlog(n))
+    Space: O(n)
+
+    Решение простое, читаемое, не требует библиотек
+
+    Временная сложность обусловлена сортировкой.  Пространственная сложность обусловлена тем,
+    что на строках sorted возвращает список символов
+
+    """
+    def is_anagram(self, s: str, t: str):
         if len(s) != len(t):
             return False
+
         return sorted(s) == sorted(t)
 
 
-solution = Solution2()
+solution2 = Solution2()
 
-assert solution.isAnagram('anagram', 'nagaram') is True
-assert solution.isAnagram('rat', 'car') is False
+assert solution2.is_anagram('anagram', 'nagaram') is True
+assert solution2.is_anagram('rat', 'car') is False
+
+
+class Solution3:
+    """Решение 3.
+
+    Time: O(nlog(n))
+    Space: O(n)
+
+    Временная сложность обусловлена сортировкой.  Пространственная сложность обусловлена
+    преобразованием строк в списки
+
+    """
+    def is_anagram(self, s, t):
+        if len(s) != len(t):
+            return False
+
+        s, t = list(s), list(t)
+
+        s.sort()
+        t.sort()
+
+        return s == t
+
+
+solution3 = Solution3()
+
+assert solution3.is_anagram('anagram', 'nagaram') is True
+assert solution3.is_anagram('rat', 'car') is False
