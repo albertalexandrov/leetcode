44d0e54718bd3809dee64ae4442aa301bee3e44d
alber.aleksandrov <alber.aleksandrov@x5.ru>
2024-11-25 12:21:32 +0300

    Переработка задачи 26

--- 26. Remove Duplicates from Sorted Array.py
+++ 26. Remove Duplicates from Sorted Array.py
@@ -50,14 +50,12 @@ class Solution(object):
         :type nums: List[int]
         :rtype: int
         """
-        left = 1
-
-        for right in range(1, len(nums)):
-            if nums[right] != nums[right - 1]:
-                nums[left] = nums[right]
-                left += 1
-
-        return left
+        i = 0
+        for j in range(1, len(nums)):
+            if nums[j] != nums[i]:
+                i += 1
+                nums[i] = nums[j]
+        return i + 1
 
 
 solution = Solution()
